1f19fbe19e648aaccb79bc851e8469fab2ed86d1
adityamahajan065 <mahajanadijamner@gmail.com>
2025-10-04 11:03:16 +0530

    Update urls.py

--- navitkare_backend/navitkare_backend/urls.py
+++ navitkare_backend/navitkare_backend/urls.py
@@ -1,10 +1,19 @@
+# urls.py
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Backend is live 🚀")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("", home),  # Root endpoint
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
